8dbbbc35968d1ce34559d4d6912c78dc14d8e3b6
Yanina Bellini Saibene <yabellini@gmail.com>
2020-07-13 18:36:07 -0300

    Update GenerarYAMLGlosario.R
    Reorder the code for better reading

--- GenerarYAMLGlosario.R
+++ GenerarYAMLGlosario.R
@@ -32,23 +32,52 @@ GlosarioT3 <- GlosarioT3 %>%
 yamlacronym <- GlosarioT3 %>%
   # tiene acronimos pero no tiene referencias
   filter(!is.na(acronym) & is.na(ref_en_a)) %>%
-  str_glue_data('- slug: {slug} \n en:\n    term: "{term_en}" \n    acronym: {acronym}\n    def: > \n {def_en} \n es:\n    term: "{term_es}" \n    acronym: {acronym}\n    def: > \n {def_es}\n')
+  str_glue_data('
+- slug: {slug}
+  en:
+    term: "{term_en}"
+    acronym: {acronym}
+    def: > 
+     {def_en}
+  es:
+    term: "{term_es}"
+    acronym: {acronym}
+    def: >
+     {def_es}
+\n')
 
 yamlacronymref <- GlosarioT3 %>%
   # tiene acronimos pero tiene referencias
   filter(!is.na(acronym) & !is.na(ref_en_a)) %>%
-  str_glue_data('- slug: {slug} \n en:\n    term: "{term_en}" \n    acronym: {acronym}\n    def: > \n {def_en} \n    ref: \n     - {ref_en_a}\n {ifelse(!is.na(ref_en_b),paste0("    - ", {ref_en_b}, "\n"),"")} es:\n    term: "{term_es}" \n    acronym: {acronym}\n    def: > \n {def_es}\n    ref: \n     - {ref_es_a}\n {ifelse(!is.na(ref_es_b),paste0("    - ", {ref_es_b}, "\n"),"")}')
+  str_glue_data('
+- slug: {slug} 
+  en:
+    term: "{term_en}" 
+    acronym: {acronym}    
+    def: > 
+     {def_en} 
+    ref:      
+      - {ref_en_a}{ifelse(!is.na(ref_en_b),paste0("\n      - ", {ref_en_b}),"")}
+  es:    
+    term: "{term_es}" 
+    acronym: {acronym}    
+    def: > 
+     {def_es}
+    ref: 
+      - {ref_es_a}
+      {ifelse(!is.na(ref_es_b),paste0("- ", {ref_es_b}"),"")}\n
+')
 
   
 yamlref <- GlosarioT3 %>%
   # tiene referencias y no tiene acrónimos
   filter(!is.na(ref_en_a) & is.na(acronym)) %>%
-  str_glue_data('- slug: {slug} \n en:\n    term: "{term_en}" \n    def: > \n {def_en} \n    ref: \n     - {ref_en_a}\n {ifelse(!is.na(ref_en_b),paste0("    - ", {ref_en_b}, "\n"),"")} es:\n    term: "{term_es}" \n        def: > \n {def_es}\n    ref: \n     - {ref_es_a}\n {ifelse(!is.na(ref_es_b),paste0("    - ", {ref_es_b}, "\n"),"")}')
+  str_glue_data('- slug: {slug} \n  en:\n    term: "{term_en}" \n    def: > \n      {def_en} \n    ref: \n     - {ref_en_a}\n {ifelse(!is.na(ref_en_b),paste0("    - ", {ref_en_b}, "\n"),"")}  es:\n    term: "{term_es}" \n        def: > \n      {def_es}\n    ref: \n     - {ref_es_a}\n {ifelse(!is.na(ref_es_b),paste0("    - ", {ref_es_b}, "\n"),"")}\n')
   
 yamldef <- GlosarioT3 %>%
   filter(is.na(acronym) & is.na(ref_en_a)) %>%
   #Solo tiene definición
-  str_glue_data('- slug: {slug} \n en:\n    term: "{term_en}" \n    def: > \n {def_en} \n es:\n    term: "{term_es}" \n    def: > \n {def_es}\n')
+  str_glue_data('- slug: {slug} \n  en:\n    term: "{term_en}" \n    def: > \n      {def_en} \n  es:\n    term: "{term_es}" \n    def: > \n     {def_es}\n\n')
 
 yaml <- c(yamlacronym, yamlacronymref, yamldef, yamldef)
 
